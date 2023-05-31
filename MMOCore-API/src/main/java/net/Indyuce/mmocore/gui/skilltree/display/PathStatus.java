178bb35d5929f40824040f58cd9882afa99d5c78
Ka0rX <guillaume.martinf@gmail.com>
2023-05-31 17:54:24 +0100

    Added the status unlockable for path in skill-tree.yml (Check wiki), make sure to reload gui/skill-tree.yml to benefit from this modification.

--- MMOCore-API/src/main/java/net/Indyuce/mmocore/gui/skilltree/display/PathStatus.java
+++ MMOCore-API/src/main/java/net/Indyuce/mmocore/gui/skilltree/display/PathStatus.java
@@ -3,5 +3,6 @@ package net.Indyuce.mmocore.gui.skilltree.display;
 public enum PathStatus {
     LOCKED,
     FULLY_LOCKED,
+    UNLOCKABLE,
     UNLOCKED;
 }
--- MMOCore-API/src/main/java/net/Indyuce/mmocore/skilltree/SkillTreePath.java
+++ MMOCore-API/src/main/java/net/Indyuce/mmocore/skilltree/SkillTreePath.java
@@ -7,9 +7,14 @@ import net.Indyuce.mmocore.skilltree.tree.SkillTree;
 public record SkillTreePath(SkillTree tree, IntegerCoordinates coordinates, SkillTreeNode from, SkillTreeNode to) {
 
     public PathStatus getStatus(PlayerData playerData) {
-        if (playerData.getNodeStatus(from) == NodeStatus.UNLOCKED && playerData.getNodeStatus(to) == NodeStatus.UNLOCKED) {
+        NodeStatus fromStatus = playerData.getNodeStatus(from);
+        NodeStatus toStatus = playerData.getNodeStatus(to);
+        if (fromStatus == NodeStatus.UNLOCKED && toStatus == NodeStatus.UNLOCKED)
             return PathStatus.UNLOCKED;
-        }
+        if ((fromStatus == NodeStatus.UNLOCKABLE && toStatus == NodeStatus.LOCKED) || (fromStatus == NodeStatus.LOCKED && toStatus == NodeStatus.UNLOCKABLE))
+            return PathStatus.UNLOCKABLE;
+        if (fromStatus == NodeStatus.FULLY_LOCKED || toStatus == NodeStatus.FULLY_LOCKED)
+            return PathStatus.FULLY_LOCKED;
         return PathStatus.LOCKED;
     }
 
