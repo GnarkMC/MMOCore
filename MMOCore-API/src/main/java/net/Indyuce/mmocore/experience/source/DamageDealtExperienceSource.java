0e5ac9ce3ad8ffb1801ebfbcf3b9d099a126c146
Jules <cym.peyrat@yahoo.fr>
2024-05-23 18:33:05 -0700

    Fixed damagedealt{} exp source crashing the server when giving a cookie to a parrot

--- MMOCore-API/src/main/java/net/Indyuce/mmocore/experience/source/DamageDealtExperienceSource.java
+++ MMOCore-API/src/main/java/net/Indyuce/mmocore/experience/source/DamageDealtExperienceSource.java
@@ -10,6 +10,7 @@ import net.Indyuce.mmocore.experience.dispenser.ExperienceDispenser;
 import net.Indyuce.mmocore.experience.source.type.SpecificExperienceSource;
 import net.Indyuce.mmocore.manager.profession.ExperienceSourceManager;
 import org.apache.commons.lang.Validate;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.event.EventHandler;
 
 import java.util.Arrays;
@@ -18,7 +19,7 @@ import java.util.stream.Collectors;
 
 import static org.bukkit.event.EventPriority.MONITOR;
 
-public class DamageDealtExperienceSource extends SpecificExperienceSource<DamageType> {
+public class DamageDealtExperienceSource extends SpecificExperienceSource<Void> {
     private final DamageType type;
 
     /**
@@ -28,14 +29,11 @@ public class DamageDealtExperienceSource extends SpecificExperienceSource<Damage
 
     public DamageDealtExperienceSource(ExperienceDispenser dispenser, MMOLineConfig config) {
         super(dispenser, config);
-        if (!config.contains("type"))
-            type = null;
+        if (!config.contains("type")) type = null;
         else {
             String str = UtilityMethods.enumName(config.getString("type"));
             //Checks if the damage type correspond to a value of the damage type enum
-            Validate.isTrue(Arrays.stream(DamageType.values()).map(Objects::toString).collect(Collectors.toList()).contains(str),
-                    "Type value not allowed. Type value allowed: magic, physical, weapon, skill, projectile," +
-                            " unarmed, on-hit, minion, dot.");
+            Validate.isTrue(Arrays.stream(DamageType.values()).map(Objects::toString).collect(Collectors.toList()).contains(str), "Type value not allowed. Type value allowed: magic, physical, weapon, skill, projectile," + " unarmed, on-hit, minion, dot.");
             type = DamageType.valueOf(str);
         }
     }
@@ -43,35 +41,26 @@ public class DamageDealtExperienceSource extends SpecificExperienceSource<Damage
     @Override
     public ExperienceSourceManager<DamageDealtExperienceSource> newManager() {
         return new ExperienceSourceManager<DamageDealtExperienceSource>() {
-            //It isn't triggered when the PlayerAttackEvent gets cancelled
-            @EventHandler(priority = MONITOR,ignoreCancelled = true)
-            public void onDamageDealt(PlayerAttackEvent e) {
-                PlayerData playerData = PlayerData.get(e.getPlayer());
-                for (DamageDealtExperienceSource source : getSources()) {
-                    double value = 0;
-                    for (DamagePacket packet : e.getDamage().getPackets()) {
-                        for (DamageType damageType : packet.getTypes()) {
-                            if (source.matchesParameter(playerData, damageType))
-                                value += packet.getFinalValue();
-                        }
 
+            @EventHandler(priority = MONITOR, ignoreCancelled = true)
+            public void onDamageDealt(PlayerAttackEvent event) {
+                final PlayerData playerData = PlayerData.get(event.getPlayer());
+                for (DamageDealtExperienceSource source : getSources())
+                    if (source.matches(playerData, null)) {
+                        double value = event.getDamage().getDamage(source.type);
+                        if (value == 0) continue;
+
+                        // Cannot count more than the entity's max health
+                        final double enemyMaxHealth = event.getEntity().getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+                        value = Math.min(value, enemyMaxHealth);
+                        source.giveExperience(playerData, value, null);
                     }
-                    source.giveExperience(playerData, value, null);
-                }
-
             }
         };
     }
 
     @Override
-    public boolean matchesParameter(PlayerData player, DamageType damageType) {
-        if (type == null) {
-            return true;
-        }
-        else {
-            return type.equals(damageType);
-
-        }
+    public boolean matchesParameter(PlayerData player, Void v) {
+        return true;
     }
-
 }
